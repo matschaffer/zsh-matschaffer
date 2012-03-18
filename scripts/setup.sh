9a827d6254f1515da2dc6294a1b598b7052d27a7
Mat Schaffer <mat@schaffer.me>
2012-03-18 00:41:24 -0400

    Add optional RW=true so I can have writable remotes

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,12 +2,18 @@
 
 set -e
 
+if [[ -n "$RW" ]]; then
+  PREFIX="git@github.com:"
+else
+  PREFIX="https://github.com/"
+fi
+
 if ! [[ -d ~/.zsh/bundle/zundle ]]; then
-  git clone http://github.com/matschaffer/zundle.git ~/.zsh/bundle/zundle
+  git clone ${PREFIX}matschaffer/zundle.git ~/.zsh/bundle/zundle
 fi
 
 if ! [[ -d ~/.zsh/bundle/zsh-matschaffer ]]; then
-  git clone http://github.com/matschaffer/zsh-matschaffer.git ~/.zsh/bundle/zsh-matschaffer
+  git clone ${PREFIX}matschaffer/zsh-matschaffer.git ~/.zsh/bundle/zsh-matschaffer
 fi
 
 test -f ~/.zshrc && mv ~/.zshrc ~/.zshrc.bak
