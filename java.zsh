48a03b6d002a76e5095e0a5cdc87f67e7e11bfc1
Mat Schaffer <matschaffer@netflix.com>
2013-10-09 15:24:38 -0700

    Use home-brew jruby

--- java.zsh
+++ java.zsh
@@ -4,3 +4,5 @@ use_java6() {
   export JAVA_HOME=/System/Library/Java/JavaVirtualMachines/1.6.0.jdk/Contents/Home
   export PATH="$JAVA_HOME/bin:$PATH"
 }
+
+export PATH="/usr/local/Cellar/jruby/1.7.4/libexec/bin:$PATH"
