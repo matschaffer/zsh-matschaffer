722b060d62ec0521a0acd12d23da8c4880bf8524
Mat Schaffer <matschaffer@netflix.com>
2013-10-09 15:24:45 -0700

    Netflix default opts

--- netflix.zsh
+++ netflix.zsh
@@ -3,4 +3,6 @@ export WORKSPACE=/Users/matschaffer/p4/matschaffer
 export P4CLIENT=matschaffer
 export P4USER=matschaffer
 
+export JAVA_OPTS="-Dnetflix.environment=test -Dnetflix.datacenter=cloud"
+
 export PATH="$HOME/netflix/nf/bin:$PATH"
