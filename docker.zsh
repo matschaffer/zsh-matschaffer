863dd9c851e56094c8c23aeb746499dcf5589610
Mat Schaffer <mat@schaffer.me>
2015-05-22 12:01:43 +0900

    a helper for boot2docker reset

--- docker.zsh
+++ docker.zsh
@@ -1,3 +1,10 @@
-export DOCKER_TLS_VERIFY=1
 export DOCKER_HOST=tcp://192.168.59.103:2376
-export DOCKER_CERT_PATH="$HOME/.boot2docker/certs/boot2docker-vm"
+export DOCKER_CERT_PATH=/Users/mat/.boot2docker/certs/boot2docker-vm
+export DOCKER_TLS_VERIFY=1
+
+reset_boot2docker() {
+  boot2docker delete
+  VBoxManage list hostonlyifs | grep -B3 192.168.59.3 | grep Name: | awk '{print $2}' | xargs -n1 VBoxManage hostonlyif remove
+  boot2docker init
+  boot2docker up
+}
