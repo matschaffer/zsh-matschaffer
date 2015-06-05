e618fd69845e386fe39e1c9fc8e0357766e4e3df
Mat Schaffer <mat@schaffer.me>
2015-06-05 10:42:33 +0900

    A retry helper

--- tools.zsh
+++ tools.zsh
@@ -1,3 +1,21 @@
+# Retry a command till it succeeds (from http://bit.ly/1GnOAhv)
+retry() {
+  local n=1
+  local max=15
+  local delay=2
+  while true; do
+    "$@" && break || {
+      if [[ $n -lt $max ]]; then
+        ((n++))
+        echo "Command failed. Attempt $n/$max:"
+        sleep $delay;
+      else
+        fail "The command has failed after $n attempts."
+      fi
+    }
+  done
+}
+
 # Bundle grep, search for text in bundled gems
 bgrep() {
   grep -r "$@" $(bundle show --paths)
